979876fbebb1d2a05e2c59ff2a76631953e1518b
Alberto Giancarlo Ferrario <albertogferrario@gmail.com>
2022-04-18 15:58:28 +0200

    Update __init__.py

--- double_ended_queue/__init__.py
+++ double_ended_queue/__init__.py
@@ -6,7 +6,7 @@
 # Generic release markers:
 #   X.Y.0   # For first release after an increment in Y
 #   X.Y.Z   # For bugfix releases
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 import collections
 from asyncio import events, locks
